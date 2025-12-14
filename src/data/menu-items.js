c6d9c631a1099fe09b408349d12ebfa3c4ee7fa8
Erik van Sebille <erikvansebille@gmail.com>
2025-12-14 20:14:31 +0100

    Blog menu item

--- src/data/menu-items.js
+++ src/data/menu-items.js
@@ -1,7 +1,8 @@
 export const menuItems = [
   { label: 'Cursus', href: '/cursus' },
   { label: 'EGOI', href: '/egoi' },
-  { label: 'In het nieuws', href: '/nieuws' },  
+  { label: 'Blog', href: '/blog' },
+  { label: 'In het nieuws', href: '/nieuws' },
   { label: 'Sponsors', href: '/#funders' },
   { label: 'Contact', href: '/contact' },
   { label: 'Informatica olympiade', href: 'https://www.informaticaolympiade.nl' },
